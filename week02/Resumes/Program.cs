d67e9a744706b6e2b9beaa34f32aa551f84eb84e
Swenson <swensonmbailey@gmail.com>
2025-05-01 13:59:24 -0500

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume userResume = new Resume();
+        userResume._jobs.Add(new Job{_company = "LeVar Law", _jobTitle = "Investigator", _startYear = 2016, _endYear = 2025});
+        userResume._jobs.Add(new Job{_company = "LDS Church", _jobTitle = "Missionary", _startYear = 2017, _endYear = 2019});
+
+        userResume.DisplayJobDetails();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,13 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayJobDetails(){
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+        foreach (Job job in _jobs){
+            job.DisplayJobDetails();
+        }
+    }
+}
