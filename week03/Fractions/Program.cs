039953313642b32f38ad3588e0cca08e835b3239
Swenson <swensonmbailey@gmail.com>
2025-05-06 21:50:27 -0500

    week 3 learning

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction(1);
+        Fraction frac3 = new Fraction(6,7);
+
+        frac1.SetTop(2);
+        frac1.SetBottom(5);
+
+        Console.WriteLine(frac1.GetFractionString() + $" = {frac1.GetDecimalValue()}");
+        Console.WriteLine(frac2.GetFractionString() + $" = {frac2.GetDecimalValue()}");
+        Console.WriteLine(frac3.GetFractionString() + $" = {frac3.GetDecimalValue()}");
+
+
     }
 }
